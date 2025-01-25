c5cbd50b8e3aed47aae093f27ae3817651af02eb
Muhammad Yusri (mhby1g21) <mhby1g21@soton.ac.uk>
2025-01-25 17:36:07 +0000

    Added delay for complete, destroy metal sheet, and fixed generated particle rotation

--- Assets/Scripts/LeakPuzzle.cs
+++ Assets/Scripts/LeakPuzzle.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections;
 
 public class LeakPuzzle : PuzzleBase
 {
@@ -93,7 +94,9 @@ public class LeakPuzzle : PuzzleBase
     {
         // Create a new GameObject for particles at the leak position
         GameObject particleObj = new GameObject($"LeakParticles_{index}");
-        particleObj.transform.position = leakAudioSources[index].transform.position;
+        particleObj.transform.position = leakPoints[index].transform.position;
+        // Rotate particles to face upwards of the leak point plane
+        particleObj.transform.rotation = Quaternion.LookRotation(leakPoints[index].transform.up);
         particleObj.transform.SetParent(leakAudioSources[index].transform);
 
         // Add and configure particle system
@@ -151,7 +154,8 @@ public class LeakPuzzle : PuzzleBase
         }
         else
         {
-            CompletePuzzle();
+            // Wait for placement sound to finish before completing puzzle
+            StartCoroutine(CompleteWithDelay());
         }
     }
 
@@ -194,6 +198,8 @@ public class LeakPuzzle : PuzzleBase
 
 
                 
+                // Destroy the interactable
+                Destroy(currentMetalSheet.gameObject);
                 OnMetalSheetPlaced(true);
             }
         }
@@ -238,6 +244,13 @@ public class LeakPuzzle : PuzzleBase
             currentLeakIndex++;
             StartCurrentLeak();
         }
+    }
+
+    private IEnumerator CompleteWithDelay()
+    {
+        // Wait for placement sound to finish (adjust time as needed based on your sound clip length)
+        yield return new WaitForSeconds(1.5f);
+        CompletePuzzle();
     }
 
     public override void CompletePuzzle()
