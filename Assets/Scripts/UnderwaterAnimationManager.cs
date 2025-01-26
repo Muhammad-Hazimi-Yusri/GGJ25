42bc1a59c996729e17974c621ce28f45bab63323
Pascal Jones (pj2g23) <pj2g23@soton.ac.uk>
2025-01-26 13:44:46 +0000

    Added ambient audio

--- Assets/Scripts/UnderwaterAnimationManager.cs
+++ Assets/Scripts/UnderwaterAnimationManager.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class UnderwaterAnimationManager : MonoBehaviour
 {
     [SerializeField] private Animator anim;
+    [SerializeField] private AudioSource audio;
+    [SerializeField] private AudioClip[] audioClips;
     [SerializeField] private float timeBetweenAnim;
+    [SerializeField] private float timeBetweenAudios;
 
     private float animCount;
+    private float audioCount;
     private bool animating;
+    private bool playingAudio;
     private string[] animBools;
     private string whichAnim;
+    private AudioClip whichAudio;
 
 
     // Start is called before the first frame update
@@ -18,6 +24,7 @@ public class UnderwaterAnimationManager : MonoBehaviour
     {
         animBools = new string[] { "fishSwim", "bigFish", "theEye" };
         animCount = 3;
+        audioCount = 5;
     }
 
     void Update()
@@ -33,6 +40,33 @@ public class UnderwaterAnimationManager : MonoBehaviour
             Debug.Log("Start Animation: " + whichAnim);
             anim.SetBool(whichAnim, true);
         }
+
+        if (audioCount > 0)
+        {
+            audioCount -= Time.deltaTime;
+        }
+        else if (!playingAudio)
+        {
+            playingAudio = true;
+            whichAudio = audioClips[Random.Range(0, audioClips.Length)];
+            StartCoroutine("playClip");
+
+        }
+    }
+
+    // Play an audio clip
+    private IEnumerator playClip()
+    {
+        Debug.Log("Start Audio: " + whichAudio.name);   
+
+        audio.clip = whichAudio;
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length);
+        audio.Stop();
+
+        Debug.Log("End Audio: " + whichAudio.name);
+        playingAudio = false;
+        audioCount = timeBetweenAudios;
     }
 
     // Call when animations are ended
